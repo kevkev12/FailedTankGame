38c716739b5f704e31beb6dffca6f06c293001c9
kevkev12 <41647403+kevkev12@users.noreply.github.com>
2018-08-02 15:58:41 -0400

    Add files via upload

---
+++ Tank.gd
@@ -0,0 +1,21 @@
+extends "res://Tanks/Tank.gd"
+
+func control(delta):
+	
+	$Turret.look_at(get_global_mouse_position())
+	
+	var rot_dir = 0
+	
+	if Input.is_action_pressed("right"):
+		rot_dir += 1
+	if Input.is_action_pressed("left"):
+		rot_dir -= 1
+	rotation += rotation_speed * rot_dir * delta
+	
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("forward"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_pressed("backwards"):
+		velocity = Vector2(-speed/2, 0).rotated(rotation)
+	
